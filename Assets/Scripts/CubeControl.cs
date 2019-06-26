b4f71ca1ab7493db0d37efa4f3c9aae795a58faa
nicruo <nicruo@gmail.com>
2019-06-26 19:19:48 +0100

    First commit;

---
+++ Assets/Scripts/CubeControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+    public string name = "hello";
+    public float force = 1;
+
+    private Rigidbody body;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(new Vector3(0,1*force,0));
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(new Vector3(0,-1*force,0));
+        }
+    }
+}
